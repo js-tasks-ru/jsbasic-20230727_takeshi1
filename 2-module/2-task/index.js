2afbcadd61a0a6c341fecf6a058669b916ceea56
Takeshi <takeshiii360@gmail.com>
2023-08-04 18:06:24 +0900

    add solutions for 2-module

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,11 @@
 function isEmpty(obj) {
-  // ваш код...
+  for (key in obj) {
+    return false;
+  }
+  return true;
 }
+
+// Второе решение через Object(keys, values, entries)
+// function isEmpty(obj) {
+//   return Object.keys(obj).length === 0;
+// }
