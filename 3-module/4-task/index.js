45b007ca2f7c250cbf63998b720a5e6e46ae531a
Takeshi <takeshiii360@gmail.com>
2023-08-08 13:55:26 +0900

    add solutions for 3-module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,5 @@
-function showSalary(users, age) {
-  // ваш код...
-}
+let showSalary = (users, age) =>
+  users
+    .filter((el) => el.age <= age)
+    .map((user) => `${user.name}, ${user.balance}`)
+    .join("\n");
