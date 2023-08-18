e0625251d212a66e9c164e8e496672dce30ea3c8
Takeshi <takeshiii360@gmail.com>
2023-08-18 15:18:34 +0900

    add solutions for 5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,43 @@
 function initCarousel() {
-  // ваш код...
+  let arrowHide = (arrow) => (arrow.style.display = "none");
+  let arrowShow = (arrow) => (arrow.style.display = "");
+  let carouselItem = document.querySelector(".carousel");
+  let carouselItemInner = document.querySelector(".carousel__inner");
+  let carouselArrowRight = document.querySelector(".carousel__arrow_right");
+  let carouselArrowLeft = document.querySelector(".carousel__arrow_left");
+  let currentSlide = 0;
+  let carouselWidth = carouselItemInner.offsetWidth;
+  carouselArrowLeft.style.display = "none";
+
+  function arrowStateChanger() {
+    if (currentSlide === 0) {
+      arrowHide(carouselArrowLeft);
+    } else {
+      arrowShow(carouselArrowLeft);
+    }
+
+    if (currentSlide === 3) {
+      arrowHide(carouselArrowRight);
+    } else {
+      arrowShow(carouselArrowRight);
+    }
+  }
+
+  carouselItem.addEventListener("click", (event) => {
+    let taget = event.target;
+
+    if (carouselArrowRight.contains(taget)) {
+      currentSlide++;
+      carouselItemInner.style.transform = `translateX(-${
+        carouselWidth * currentSlide
+      }px)`;
+    } else if (carouselArrowLeft.contains(taget)) {
+      currentSlide--;
+      carouselItemInner.style.transform = `translateX(-${
+        carouselWidth * currentSlide
+      }px)`;
+    }
+
+    arrowStateChanger();
+  });
 }
